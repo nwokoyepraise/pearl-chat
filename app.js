ed99aa950e8a312313b702575964961c2e7191ef
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-31 00:12:42 +0100

    minor bug fix to create server

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const port_number = 3000;
- 
 
-app.listen(port_number, () => {
+server.listen(port_number, () => {
     console.log(`server listening on port ${port_number}`)
 })
