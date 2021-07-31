1c60a3ccfb7135c1a9ac866b8d3cadab24fd4cd7
nwokoyepraise <nwokoyepraise@gmail.com>
2021-07-31 10:23:30 +0100

    route to serve html to create and join room

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const port_number = 3000;
+const io = require('socket.io')(server);
+
+app.use(express.static("public"));
+
+try {
+
+    app.get('/', (req, res) => res.sendFile(__dirname + '/chat.html'));
+
+} catch (error) {
+    console.error(error)
+}
 
 server.listen(port_number, () => {
     console.log(`server listening on port ${port_number}`)
