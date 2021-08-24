4d4a0dde004234a6edfc343dabc80cf5fafc0176
nwokoyepraise <nwokoyepraise@gmail.com>
2021-08-24 23:57:21 +0100

    restructure connection and chat workflow

--- app.js
+++ app.js
@@ -8,20 +8,33 @@ app.use(express.static("public"));
 
 try {
 
-    app.get('/', (req, res) => res.sendFile(__dirname + '/chat.html'));
+    app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+    app.get('/chat', (req, res) => res.sendFile(__dirname + '/chat.html'));
+
+    const namespace = io.of('/chat');
 
     io.on('connection', client => {
         console.log('user connected')
-        client.on('create_room', (room_number)=>{
-            console.log(`client ${client.id} created room ${room_number}!`);
-            client.join(room_number);
+        client.on('create_room', (room_number) => {
+            console.log(`client ${client.id} created and redirected to room ${room_number}!`);
+            client.emit('redirect', room_number);
         });
-        client.on('join_room', (room_number)=>{
-            client.join(room_number);
-            console.log(`client ${client.id} joined room ${room_number}!`)
+        client.on('join_room', (room_number) => {
+            console.log(`client ${client.id} joined and redirected to room ${room_number}!`);
+            client.emit('redirect', room_number);
         });
         // client.on('event', data => { /* … */ });
         client.on('disconnect', () => { console.log(`client ${client.id} disconnected!`) });
+    });
+
+    namespace.on('connection', client => {
+        client.on('join_room', (room_number) => {
+            client.join(room_number);
+            console.log(`client ${client.id} joined room ${room_number}!`);
+        });
+        client.on('message', message => {
+            namespace.emit('message', message);
+        });
     });
 
 } catch (error) {
