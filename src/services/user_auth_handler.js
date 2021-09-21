81f6a8480cbd78e76176e98d5cbc2c5d52cafd5f
nwokoyepraise <nwokoyepraise@gmail.com>
2021-09-21 23:52:54 +0100

    add login handler

---
+++ src/services/user_auth_handler.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const token_handle = require('../utils/token_handle');
+const user_profile_model = require('../models/user_profile_model');
+const argon2 = require('argon2');
+
+module.exports.login = async function (user) {
+    try {
+        let res0 = await user_profile_model.get_profile_data('email', user.email);
+
+        //return if any credential is null
+        if (!user.email || !user.password) { return { status: false, status_code: 400, message: "Null values not allowed!" } }
+
+        //unhash and verify password
+        if (!(await argon2.verify(res0.password, user.password))) { return { status: false, status_code: 401, message: "Invalid Credentials!" } }
+
+        //generate new user tokens
+        const jwt = token_handle.gen_jwt({ sub: res0.user_id });
+
+        //return new credentials
+        return { status: true, data: { user_id: user.user_id, jwt: jwt } }
+    } catch (error) {
+        console.error(error);
+        return { status: false, status_code: 500, message: 'Internal Server Error' }
+    }
+}
