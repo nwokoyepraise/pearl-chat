746cb562e730dad1915a1eb874f16d3e7d4c810c
nwokoyepraise <nwokoyepraise@gmail.com>
2022-01-03 12:34:29 +0100

    add onclick to button

--- public/js/insta_chat/intro_button.js
+++ public/js/insta_chat/intro_button.js
@@ -1,6 +1,10 @@
 const btn_new = document.querySelector('button.new-meeting');
-var body_overlay = document.getElementById("mo");
+
 
 function new_meeting() {
+    document.getElementById("body-overlay").style.display = "block";
+}
+
+function join() {
     document.getElementById("body-overlay").style.display = "block";
 }
