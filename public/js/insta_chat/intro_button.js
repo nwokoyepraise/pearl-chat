68b73c9d8cf29a0fdf99ccc7546f0b736b48d9ee
nwokoyepraise <nwokoyepraise@gmail.com>
2022-01-03 12:31:59 +0100

    add overlay on button click

---
+++ public/js/insta_chat/intro_button.js
@@ -0,0 +1,6 @@
+const btn_new = document.querySelector('button.new-meeting');
+var body_overlay = document.getElementById("mo");
+
+function new_meeting() {
+    document.getElementById("body-overlay").style.display = "block";
+}
